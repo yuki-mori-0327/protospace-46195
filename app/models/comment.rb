049cc0c9785f5e2c693c60ac201b82f327711043
yuki-mori-0125 <01252000yuuki@gmail.com>
2025-08-12 15:15:18 +0900

    Revert ".first commit"
    This reverts commit c1d56c3a5d899d25db862a5b1d0ca2490cfdb48d.

--- app/models/comment.rb
+++
@@ -1,6 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :prototype
-
-  validates :text, presence: true
-end
--- test/controllers/prototypes_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class PrototypesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get prototypes_index_url
-    assert_response :success
-  end
-end
